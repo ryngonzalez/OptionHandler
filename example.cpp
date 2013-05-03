e2ddd9af2cf4bb5f6d21bf0d7602a1de139a202a
hning <hning@umich.edu>
2013-05-03 16:59:21 -0400

    Before pull

--- example.cpp
+++ example.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 #include "option_handler.h"
 
 int main(int argc, char *argv[])
@@ -8,14 +9,14 @@ int main(int argc, char *argv[])
   OptionHandler::Handler h = OptionHandler::Handler(argc, argv);
 
   // Setup options
-  h.add_option('m', "mode", OptionHandler::ArgumentType::REQUIRED, false);
+  h.add_option('m', "mode", OptionHandler::ArgumentType::REQUIRED, true);
   h.add_option('h', "help", OptionHandler::ArgumentType::NONE,     false);
 
   // Check if options were passed to the program
   bool help = h.get_option("help");
   bool mode_set = h.get_option("mode");
   // Get the argument passed to a particular option
-  std::string mode = h.get_argument("mode");
+  std::vector<std::string> mode = h.get_arguments("mode");
 
   // Check if 'help' option was set
   if (help)
@@ -29,7 +30,11 @@ int main(int argc, char *argv[])
 
   // Check the values
   std::cout << help     << std::endl;
-  std::cout << mode     << std::endl;
+  for(auto i : mode)
+  {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
   std::cout << mode_set << std::endl;
 
   return 0;
