3675487e179b1d164364b3966f18ff21af9b0bca
ryngonzalez <ryan@ionizedmedia.com>
2013-05-03 14:05:00 -0400

    Update example to use new api.

--- example.cpp
+++ example.cpp
@@ -2,33 +2,35 @@
 #include <iostream>
 #include "option_handler.h"
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
-  // Define the options you want to handle.
-  const struct option options[5] = {
-    {"output",  required_argument,  0, 'o'},
-    {"stack",   no_argument,        0, 's'},
-    {"queue",   no_argument,        0, 'q'},
-    {"help",    no_argument,        0, 'h'},
-    {0, 0, 0, 0}
-  };
 
-  // Create the option handler
-  OptionHandler h = OptionHandler(argc, argv, options, 5);
+  OptionHandler::Handler h = OptionHandler::Handler(argc, argv);
 
-  // Get a bool indicating whether a flag was set
-  bool s = h.get_option('s'),
-       q = h.get_option('q');
+  // Setup options
+  h.add_option('m', "mode", OptionHandler::ArgumentType::REQUIRED, false);
+  h.add_option('h', "help", OptionHandler::ArgumentType::NONE,     false);
 
- // Get the argument passed to the long or short flag
-  string o = h.get_argument('o');
+  // Check if options were passed to the program
+  bool help = h.get_option("help");
+  bool mode_set = h.get_option("mode");
+  // Get the argument passed to a particular option
+  std::string mode = h.get_argument("mode");
 
-  // Get your return values
-  cout << s << endl;
-  cout << q << endl;
-  cout << o << endl;
+  // Check if 'help' option was set
+  if (help)
+    std::cout << "The options for this program are..." << std::endl;
+
+  // Make sure 'mode' option is set and that an argument was provided to it.
+  if (!mode_set || mode == "") {
+    std::cerr << "Mode must be set.\n";
+    exit(1);
+  }
+
+  // Check the values
+  std::cout << help     << std::endl;
+  std::cout << mode     << std::endl;
+  std::cout << mode_set << std::endl;
 
   return 0;
 }
